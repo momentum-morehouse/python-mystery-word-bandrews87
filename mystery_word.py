b2bfecdc810927d14f70d2eddbcedf7908afde67
bandrews87 <70330631+bandrews87@users.noreply.github.com>
2020-10-04 21:51:57 -0500

    mystery word

---
+++ mystery_word.py
@@ -0,0 +1,17 @@
+word = "love"
+# MUSTHAVE
+# TODO user makes a guess
+
+# TODO let us know if answer was correct or incorrect
+# TODO user will see a letter if answer is correct, if answer is incorrect it will be blank
+# TODO keep track of the guesses
+# TODO game ends if user makes 8 wrong guesses
+# TODO game ends if user wins the game
+# NICETOHAVE
+# TODO user selects level of difficulty
+# TODO have computer pick randomly from list
+# TODO let the user know how many letters the word contains
+print(word)
+file = open("words.txt", "r")
+print(file)
+print(file.readlines())
